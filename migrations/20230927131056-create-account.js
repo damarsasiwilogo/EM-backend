52f823f29a4301b59c95b3fbd1ddff68d650983f
damarsasiwilogo <damarsasiwilogo@gmail.com>
2023-09-27 20:25:21 +0700

    add models account and referral

---
+++ migrations/20230927131056-create-account.js
@@ -0,0 +1,53 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Accounts", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      referralId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Referrals",
+          key: "id",
+        },
+      },
+      accountType: {
+        type: Sequelize.ENUM("User", "Event Organizer"),
+      },
+      username: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+      },
+      phoneNumber: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Accounts");
+  },
+};
---
+++ models/account.js
@@ -0,0 +1,30 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Account extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Account.hasOne(models.Referral, { foreignKey: "referralId" });
+    }
+  }
+  Account.init(
+    {
+      accountType: DataTypes.ENUM("User", "Event Organizer"),
+      username: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      phoneNumber: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Account",
+    }
+  );
+  return Account;
+};
