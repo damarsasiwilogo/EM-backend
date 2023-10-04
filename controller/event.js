04a57eae35d6608a747085467e25ad1e52acf543
ActualRML <rmlilipaly@gmail.com>
2023-10-04 16:05:19 +0700

    tambahin

--- controller/event.js
+++ controller/event.js
@@ -3,11 +3,11 @@ const bcrypt = require("bcryptjs");
 const { Account, Referral, Event } = require("../models");
 const { Op } = require("sequelize");
 const event = require("../models/event");
+const account = require("../models/account");
 
 exports.handleCreateEvent = async (req, res) => {
   const {
     eventName,
-    accountId,
     type,
     location,
     description,
@@ -16,6 +16,7 @@ exports.handleCreateEvent = async (req, res) => {
     gold_ticket_price,
     premium_ticket_price,
   } = req.body;
+  const accountId = req.user.id;
   const { filename } = req.file;
 
   try {
@@ -58,17 +59,26 @@ exports.handleCreateEvent = async (req, res) => {
 
 exports.getAllEvents = async (req, res) => {
   try {
+    const where = {};
+    const { eventName } = req.query;
+    if (eventName) {
+      where.eventName = {
+        [Op.like]: `%${eventName}%`,
+      };
+    }
+
     const events = await Event.findAll({
+      where,
       include: [
         {
           model: Account,
-          attributes: ["username"],
+          attributes: ["firstName"],
         },
       ],
     });
     //transform the data
     const responseObj = events.map((event) => ({
-      eventName: event.name,
+      eventName: event.eventName,
       accountId: event.accountId,
       type: event.type,
       location: event.location,
@@ -80,6 +90,44 @@ exports.getAllEvents = async (req, res) => {
       event_img: event.event_img,
     }));
 
+    res.status(200).json({
+      ok: true,
+      data: responseObj,
+    });
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: String(error),
+    });
+  }
+};
+exports.getEventId = async (req, res) => {
+  try {
+    const { accountId } = req.params;
+    const datas = await Event.findAll({
+      where: {
+        accountId,
+      },
+      include: [
+        {
+          model: Account,
+          attributes: ["firstName"],
+        },
+      ],
+    });
+    const responseObj = datas.map((event) => ({
+      eventName: event.eventName,
+      accountId: event.accountId,
+      type: event.type,
+      location: event.location,
+      description: event.description,
+      date_time: event.date_time,
+      silver_ticket_price: event.silver_ticket_price,
+      gold_ticket_price: event.gold_ticket_price,
+      premium_ticket_price: event.premium_ticket_price,
+      event_img: event.event_img,
+      createdBy: event.Account.firstName,
+    }));
     res.status(200).json({
       ok: true,
       data: responseObj,
